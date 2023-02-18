2910faecdfca972805167392db6840d943d0a847
BALABooL <edman0986@gmail.com>
2023-02-18 20:12:37 +0300

    script fix in html

--- scripts/script.js
+++ scripts/script.js
@@ -11,17 +11,46 @@ function languageToggle() {
   }
 }
 
-function buttonBackground() {
-  let buttonClasses = document.getElementsByClassName("button").classList;
-  console.log(buttonClasses);
-  // let colorBarClasses = document.getElementById("color_bar").classList;
-  // if (buttonClasses.contains("col1") ) {
-  //   buttonClasses.remove("col1");
-  //   colorBarClasses.add("invisible");
-  //   colorBarClasses.add("col1");
-  // } else {
-  //   buttonClasses.add("col1");
-  //   colorBarClasses.remove("invisible");
-  //   colorBarClasses.add("col1");
-  // }
+function buttonBackground(number) {
+  if (number == 1) {
+    let buttonClasses = document.getElementById("button_col1").classList;
+    let colorBarClasses = document.getElementById("color_bar").classList;
+    if (buttonClasses.contains("col1")) {
+      buttonClasses.remove("col1");
+      colorBarClasses.add("invisible");
+      colorBarClasses.add("col1");
+    } else {
+      buttonClasses.add("col1");
+      colorBarClasses.remove("invisible");
+      colorBarClasses.add("col1");
+    }
+  } 
+  if (number == 2) {
+    let buttonClasses = document.getElementById("button_col2").classList;
+    let colorBarClasses = document.getElementById("color_bar").classList;
+    if (buttonClasses.contains("col2")) {
+      buttonClasses.remove("col2");
+      colorBarClasses.add("invisible");
+    } else {
+      buttonClasses.add("col2");
+      colorBarClasses.remove("invisible");
+      colorBarClasses.add("col2");
+    }
+  }
 }
+
+// плохой способ
+
+// function buttonBackground1() {
+//   let buttonClasses = document.getElementById("button_col1").classList;
+//   let colorBarClasses = document.getElementById("color_bar").classList;
+//   if (buttonClasses.contains("col1")) {
+//     buttonClasses.remove("col1");
+//     colorBarClasses.add("invisible");
+//     colorBarClasses.add("col1");
+//   } else {
+//     buttonClasses.add("col1");
+//     colorBarClasses.remove("invisible");
+//     colorBarClasses.add("col1");
+//   }
+// }
