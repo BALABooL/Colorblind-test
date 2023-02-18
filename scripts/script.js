95e1abbc4832c2269d7d8b6cf1a6f855a9bcca64
BALABooL <edman0986@gmail.com>
2023-02-18 20:54:31 +0300

    script doesn't work

--- scripts/script.js
+++ scripts/script.js
@@ -1,56 +1,39 @@
 // описание функции, позволяющей менять язык текста на странице
 function languageToggle() {
-  let langButtonClasses = document.getElementById("language").classList;
-  console.log(langButtonClasses);
-  if (langButtonClasses.contains("RUS")) {
-    langButtonClasses.remove("RUS");
-    langButtonClasses.add("UK");
+  let langBtnClasses = document.getElementById("language").classList;
+  console.log(langBtnClasses);
+  if (langBtnClasses.contains("RUS")) {
+    langBtnClasses.remove("RUS");
+    langBtnClasses.add("UK");
   } else {
-    langButtonClasses.remove("UK");
-    langButtonClasses.add("RUS");
+    langBtnClasses.remove("UK");
+    langBtnClasses.add("RUS");
   }
 }
-
-function buttonBackground(number) {
-  if (number == 1) {
-    let buttonClasses = document.getElementById("button_col1").classList;
-    let colorBarClasses = document.getElementById("color_bar").classList;
-    if (buttonClasses.contains("col1")) {
-      buttonClasses.remove("col1");
-      colorBarClasses.add("invisible");
-      colorBarClasses.add("col1");
-    } else {
-      buttonClasses.add("col1");
-      colorBarClasses.remove("invisible");
-      colorBarClasses.add("col1");
-    }
-  } 
-  if (number == 2) {
-    let buttonClasses = document.getElementById("button_col2").classList;
-    let colorBarClasses = document.getElementById("color_bar").classList;
-    if (buttonClasses.contains("col2")) {
-      buttonClasses.remove("col2");
-      colorBarClasses.add("invisible");
-    } else {
-      buttonClasses.add("col2");
-      colorBarClasses.remove("invisible");
-      colorBarClasses.add("col2");
-    }
-  }
-}
-
-// плохой способ
-
-// function buttonBackground1() {
-//   let buttonClasses = document.getElementById("button_col1").classList;
-//   let colorBarClasses = document.getElementById("color_bar").classList;
-//   if (buttonClasses.contains("col1")) {
-//     buttonClasses.remove("col1");
-//     colorBarClasses.add("invisible");
-//     colorBarClasses.add("col1");
-//   } else {
-//     buttonClasses.add("col1");
-//     colorBarClasses.remove("invisible");
-//     colorBarClasses.add("col1");
-//   }
+// number - код цветового спектра
+// function btnBackground(number) {
+  // if (number == 1) {
+  //   let btnClasses = document.getElementById("button_col1").classList;
+  //   let colorBarClasses = document.getElementById("color_bar").classList;
+  //   if (btnClasses.contains("col1")) {
+  //     btnClasses.remove("col1");
+  //     colorBarClasses.add("invisible");
+  //   } else {
+  //     btnClasses.add("col1");
+  //     colorBarClasses.remove("invisible");
+  //     colorBarClasses.add("col1");
+  //   }
+  // } 
+  // if (number == 2) {
+  //   let btnClasses = document.getElementById("button_col2").classList;
+  //   let colorBarClasses = document.getElementById("color_bar").classList;
+  //   if (btnClasses.contains("col2")) {
+  //     btnClasses.remove("col2");
+  //     colorBarClasses.add("invisible");
+  //   } else {
+  //     btnClasses.add("col2");
+  //     colorBarClasses.remove("invisible");
+  //     colorBarClasses.add("col2");
+  //   }
+  // }
 // }
