67d3c9beae2e76664dd43dab175f7d836b366c4d
BALABooL <edman0986@gmail.com>
2023-03-05 21:31:28 +0300

    adding broken func

--- scripts/script.js
+++ scripts/script.js
@@ -11,29 +11,31 @@ function languageToggle() {
   }
 }
 // number - код цветового спектра
-// function btnBackground(number) {
-  // if (number == 1) {
-  //   let btnClasses = document.getElementById("button_col1").classList;
-  //   let colorBarClasses = document.getElementById("color_bar").classList;
-  //   if (btnClasses.contains("col1")) {
-  //     btnClasses.remove("col1");
-  //     colorBarClasses.add("invisible");
-  //   } else {
-  //     btnClasses.add("col1");
-  //     colorBarClasses.remove("invisible");
-  //     colorBarClasses.add("col1");
-  //   }
-  // } 
-  // if (number == 2) {
-  //   let btnClasses = document.getElementById("button_col2").classList;
-  //   let colorBarClasses = document.getElementById("color_bar").classList;
-  //   if (btnClasses.contains("col2")) {
-  //     btnClasses.remove("col2");
-  //     colorBarClasses.add("invisible");
-  //   } else {
-  //     btnClasses.add("col2");
-  //     colorBarClasses.remove("invisible");
-  //     colorBarClasses.add("col2");
-  //   }
-  // }
-// }
+// расписано только для двух кнопок
+function btnBackground(number) {
+  if (number == 1) {
+    let btnClasses = document.getElementById("button_col1").classList;
+    let colorBarClasses = document.getElementById("color_bar").classList;
+    if (btnClasses.contains("col1")) {
+      btnClasses.remove("col1");
+      colorBarClasses.add("invisible");
+    } else {
+      btnClasses.add("col1");
+      colorBarClasses.remove("invisible");
+      colorBarClasses.add("col1");
+    }
+  } 
+  if (number == 2) {
+    let btnClasses = document.getElementById("button_col2").classList;
+    let colorBarClasses = document.getElementById("color_bar").classList;
+    if (btnClasses.contains("col2")) {
+      btnClasses.remove("col2");
+      colorBarClasses.add("invisible");
+    } else {
+      btnClasses.add("col2");
+      colorBarClasses.remove("invisible");
+      colorBarClasses.add("col2");
+    }
+  }
+}
+// функция работает неправильно, нужно переработать
